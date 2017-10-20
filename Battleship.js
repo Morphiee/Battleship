ce4324cbb076bdb3e3e2744d59e5dcd163342c70
learn <learn@learn06.local>
2017-10-20 15:18:49 -0700

    Added code to stop ships from being ontop or next to each toher. (Broken need to fix)

--- Battleship.js
+++ Battleship.js
@@ -1,5 +1,16 @@
 var board = [[], [], [], [], [], [], [], [], [], []]
 var hitCounter = 0;
+var shipCoordinates = [];
+function generateCellNumbers() {
+    var rowNum = randomNum();
+    var cellNum = randomNum();
+    return "" + rowNum + cellNum;
+};
+    // create a function that generates a new random number every time it is called
+function randomNum() {
+    var randomCounterRow = Math.floor(Math.random() * 9);
+    return randomCounterRow;
+}
 
 $(document).ready(function() {
     var torpedoes = 0;
@@ -14,29 +25,33 @@ $(document).ready(function() {
         }
     }
 
-    // create a function that generates a new random number every time it is called
-    var randomCounterRow;
 
-    function randomNum() {
-        randomCounterRow = Math.floor(Math.random() * 9);
-        return randomCounterRow;
-    }
 // changes board array at row and cell location to hold a ship
     for (var count = 0; count < 5; count++) {
         // save random number for row to a var
-        randomNum();
-        var rowNum = randomCounterRow;
+        var rowNum = randomNum();
         // save a random number for cell to a var
-        randomNum();
-        var cellNum = randomCounterRow;
+        var cellNum = randomNum();
         // board[varRow[varCell]]
 
         board[rowNum][cellNum] = ship;
 
+        generateCellNumbers(5);
         console.log(test);
-
+        // if it has class joined, remove 1 from counter
+        if ($(this).hasClass("joined" || "ship")) {
+            count -=1;
+        };
         var joined = "#" + rowNum.toString() + cellNum.toString();
-        $(joined).addClass("joined");
+        $(joined).addClass("ship");
+        var aboveShip = "#" + (rowNum - 1).toString()+ cellNum.toString();
+        $(aboveShip).addClass("joined");
+        var belowShip = "#" + (rowNum + 1).toString() + cellNum.toString();
+        $(belowShip).addClass("joined");
+        var leftOfShip = "#" + rowNum.toString() + (cellNum - 1).toString();
+        $(leftOfShip).addClass("joined");
+        var rightOfShip = "#" + rowNum.toString() + (cellNum + 1).toString();
+        $(rightOfShip).addClass("joined");
 
         var test = "#" + rowNum + cellNum;
 
@@ -44,6 +59,25 @@ $(document).ready(function() {
 
         console.log(board);
     }
+
+    // function to get starting point of ship. 1 parameter takes the length of the ship. In function, take the coordinates, iterate through the length of the ship, and check to see if all the next blocks have a ship. if so, start over.
+
+
+    function createHorizontalShip(length) {
+        var newCoordinates = generateCellNumbers();
+        // var shipCoordinates = [];
+        var rowNum = newCoordinates[0]
+        var cellNum = newCoordinates[1]
+        for (var l = 0; l < length; l++) {
+            var checkedSpot = "#" + rowNum.toString() + cellNum.toString();
+            if ($(checkedSpot).hasClass("ship")) {
+                newCoordinates = generateCellNumbers();
+            } else {
+                $(shipCoordinates).prepend(newCoordinates);
+                rowNum += 1;
+            }
+        }
+}
 
     $("td").on("click", function() {
         var index = $(this).attr("class");
@@ -54,7 +88,7 @@ $(document).ready(function() {
 
         // if (ship == board[index[0]][index[1]])
 
-        if ($(this).hasClass("joined")) {
+        if ($(this).hasClass("ship")) {
             $(".hitOrMiss").text("Hit!");
             $(this).addClass("hit");
             hitCounter += 1;
@@ -65,7 +99,7 @@ $(document).ready(function() {
         $(this).off("click");
         if (torpedoes > 24) {
             $(this).addClass("lost");
-            $(".joined").addClass("hit");
+            $(".ship").addClass("hit");
             $("td").off("click");
             $(".hitOrMiss").text("You have no more torpedoes! You lose :(")
             // location.reload();
